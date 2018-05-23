4b30b81d563068b0d6cf8b809c3b04a7594a4644
flandre8233 <flandre8233@gmail.com>
2018-05-24 02:53:41 +0800

    add pathfinding

---
+++ Assets/npcController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class npcController : MonoBehaviour {
+    NavMeshAgent navMeshAgent;
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        navMeshAgent.SetDestination(playermovement.instance.transform.position);
+
+    }
+}
